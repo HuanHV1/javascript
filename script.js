c7d5a1424e3d53703ca3f9ec790ed6314ce7fb0b
HuanHV1 <huan.21.no1@gmail.com>
2018-02-02 20:13:00 +0700

    update template

---
+++ script.js
@@ -0,0 +1,8 @@
+window.addEventListener("DOMContentLoaded", function () {
+    var btn = document.getElementById('test');
+    console.log(btn);
+
+    btn.onclick = function () {
+        alert("JavaScript Ok");
+    }
+}, false)
